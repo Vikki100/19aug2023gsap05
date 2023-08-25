6deee049af9f6450ee8e4f3aaaed7efb649bd8ca
Vikki100 <merasafar.13safar@gmail.com>
2023-08-26 03:38:35 +0530

    gsap learing

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-gsap.to('#page1 h1',{
+
+
+
+
+function time() {
+    let a = 0;
+    
+    setInterval(() => {
+        a = a + Math.floor(Math.random()*10)
+   
+        if (a<100) {
+            document.querySelector('#loader h1').innerHTML = a +'%'
+        } else {
+            a = 100;
+            document.querySelector('#loader h1').innerHTML = a+'%' 
+            
+        }       
+    }, 100);
+}
+
+
+
+
+ let tl =gsap.timeline()
+
+ tl.from('#loader h1',{
+    scale:0.5,
+    duration:2,
+    delay:0.4,
+    onStart: time()
+
+ })
+
+ tl.to("#loader",{
+    y:-1000,
+    duration:2,
+    delay:0.4
+ })
+
+
+tl.to('#page1 h1',{
     Transform:'translateX(-100%)',
-    fontWeight:'200',
+    fontWeight:'150',
+    
     scrollTrigger:{
         trigger:"#page1",
         scroller:"body",
